67c86f2d516d28b4e12c13c6e090532822231ba5
ElementJason <jason@elementanimation.com>
2026-06-01 20:50:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,23 @@ def get_headers():
 
 
 def get_broadcaster_id():
-    url = f"https://api.twitch.tv/helix/users?login={CHANNEL_NAME}"
+    url = f"https://api.twitch.tv/helix/users?login=warframe"
+
     r = requests.get(url, headers=get_headers())
     data = r.json()
+
+    print("USER API RESPONSE:", data)
+
+    if "data" not in data or len(data["data"]) == 0:
+        raise Exception("Failed to fetch broadcaster ID")
+
     return data["data"][0]["id"]
 
 
 def get_schedule():
     broadcaster_id = get_broadcaster_id()
+
+    print("BROADCASTER ID:", broadcaster_id)
 
     url = f"https://api.twitch.tv/helix/schedule?broadcaster_id={broadcaster_id}"
 
@@ -35,6 +44,3 @@ def get_schedule():
     r.raise_for_status()
 
     return r.json().get("data", {}).get("segments", [])
-
-
-segments = get_schedule()
